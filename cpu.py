c225b95254b89b5fb9e5bc0fd027a12be2fb87de
Tomas Pospisek <tpo@sourcepole.ch>
2024-11-22 16:39:11 +0100

    use semantic symbols for special addresses inside the storage

--- cpu.py
+++ cpu.py
@@ -1,17 +1,27 @@
 import threading
 import time
 
+# INDEX_OF_OTHER_PROCESS_ADR
+#          |
+#          | PROCESS_ID_ADR                                                                              IRET_DESTINATION_ADR
+#          | |                                                                                                     |
+#          v v                                                                                                     v
 storage = [8,0,"NOP","NOP","NOP","NOP", "JMP", 2, "NOP", "NOP", "NOP", "NOP", "NOP", "JMP", 8,"ISR", "TGL", "IRET",0]
 ir = False
 index = 2
+
+# TODO: allow IRET_DESTINATION to be at different places
+IRET_DESTINATION_ADR=len(storage)-1
+PROCESS_ID_ADR=1
+INDEX_OF_OTHER_PROCESS_ADR=0
 
 def execute():
     global index
     global ir
     while True:
         if ir: 
-            storage[len(storage)-1]=storage[0]
-            storage[0]=index
+            storage[IRET_DESTINATION_ADR]=storage[INDEX_OF_OTHER_PROCESS_ADR]
+            storage[INDEX_OF_OTHER_PROCESS_ADR]=index
             index = 15
             ir = False
 
@@ -39,17 +49,17 @@ def executeJMP():
 
 def executeTGL():
     global index
-    if storage[1]==0:
-        storage[1]=1
+    if storage[PROCESS_ID_ADR]==0:
+        storage[PROCESS_ID_ADR]=1
     else: 
-        storage[1]=0 
+        storage[PROCESS_ID_ADR]=0 
     print("TGL: toggle process") 
     index +=1
     
 def executeIRET():
     global index
     global ir
-    index = storage[index+1]
+    index = storage[IRET_DESTINATION_ADR]
     print(f"IRET: IR routine done, going to position: {index} of Process {storage[1]}")
 
 def executeISR():
